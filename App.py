44f447da884ef418d4dbb35bc3d8f1d401daf01a
unknown <mig.garzon@udla.edu.co>
2023-01-15 19:17:30 -0500

    Signed-off-by: unknown <mig.garzon@udla.edu.co>

--- App.py
+++ App.py
@@ -4,12 +4,14 @@ import flask
 from flask import Flask, render_template, request
 from routes.Subir import Subir_blue
 from routes.Predicion import Predicion_blue
+from routes.Crud import Crud_blue
 from flask_sqlalchemy import SQLAlchemy
 
 # creating instance of the class
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/ predicion'
+app.secret_key="1"
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/predicion'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 SQLAlchemy(app)
 
@@ -19,7 +21,7 @@ def index():
 
 app.register_blueprint(Subir_blue)
 app.register_blueprint(Predicion_blue)
-
+app.register_blueprint(Crud_blue)
 
 
 
--- index.py
+++ index.py
@@ -1,4 +1,8 @@
 from App import app
+from utils.db import db
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 if __name__=="__main__":
 
