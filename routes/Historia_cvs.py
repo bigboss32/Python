bb5dd63725fc8ac75e44b54fbe8b267ac2f2d64d
unknown <mig.garzon@udla.edu.co>
2023-03-03 13:18:02 -0500

    Signed-off-by: unknown <mig.garzon@udla.edu.co>

--- routes/Historia_cvs.py
+++ routes/Historia_cvs.py
@@ -1,11 +1,27 @@
 from flask import Blueprint,request,render_template
-
-
+import os
+import time
 historial_blue=Blueprint('Historia_cvs',__name__)
 
 
 @historial_blue.route('/Historia_cvs')
 def historial():
- return render_template("simple.html")
+        a=0
+        ejemplo_dir = 'Archivo'
+        contenido = os.listdir(ejemplo_dir)
+        archivos = []
+        tiempo = []
+        for fichero in contenido:
+            if os.path.isfile(os.path.join(ejemplo_dir, fichero)) and fichero.endswith('.csv'):
+                archivos.append(fichero)
+                print(fichero)
+                ti_c = os.path.getctime(ejemplo_dir+"/"+fichero)
+                c_ti = time.ctime(ti_c)
+                tiempo.append(c_ti)
+      
+        
+        print(archivos)
+        print(tiempo)
+        return render_template("simple.html",archivos=archivos,fecha=tiempo)
 
    
