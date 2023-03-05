b4bdf9a788ea2237944f2cd3b92ad2199d052eb3
unknown <mig.garzon@udla.edu.co>
2023-03-05 10:14:02 -0500

    Signed-off-by: unknown <mig.garzon@udla.edu.co>

--- Modelo/base.sql
+++
@@ -1,9 +0,0 @@
-use Proyecto;
-
-select * from registro order by numero_entrega asc limit 1;
-select * from registro;
-SELECT  nombre FROM registro;
-select count(*) from registro;
-SELECT COUNT(*) FROM registro WHERE FKresultado = 0
-
-
